54da8e80ee1fd042d601bdc8b4e4d6fe160dc8e0
Yasir <code.yasir@gmail.com>
2022-04-10 09:31:11 +0300

    add menu component logic

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ data }) => {
+  return (
+    <div className="section-center">
+      {data.map((item) => {
+        const { id, img, title, price, desc } = item;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
-
 export default Menu;
